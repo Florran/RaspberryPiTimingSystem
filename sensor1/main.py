d41fdc8460faf1d64e132959c025fd077e54ffd0
Florran <florenius05@gmail.com>
2024-03-25 17:11:29 +0100

    Fixed timer and motion detection

--- sensor1/main.py
+++ sensor1/main.py
@@ -6,10 +6,12 @@ import threading
 pir = MotionSensor(4)
 app = Flask(__name__)
 monitoring = False
+motion_detected = False
 
 @app.route('/handle_request', methods=['GET'])
 def handle_request():
     global monitoring
+    global motion_detected
     request_data = request.args.get('data')
 
     if request_data == "activate" and not monitoring:
@@ -26,11 +28,14 @@ def handle_request():
     return 'Invalid command or already in desired state.'
 
 def monitor_motion():
-    start_time = time.time() * 1000
+    global motion_detected
     while monitoring:
         if pir.motion_detected:
             print("Motion detected!")
-            timer(start_time)
+            motion_detected = True
+            timer()
+            motion_detected = False
+        time.sleep(0.1)
 
 def format_time(milliseconds):
     hours = milliseconds // 3600000
@@ -39,8 +44,9 @@ def format_time(milliseconds):
     milliseconds = milliseconds % 1000
     return f"{hours:02}:{minutes:02}:{seconds:02}.{milliseconds:03}"
 
-def timer(start_time):
-    while monitoring:
+def timer():
+    start_time = time.time() * 1000
+    while motion_detected:
         elapsed_time = int((time.time() * 1000) - start_time)
         print(format_time(elapsed_time), end='\r')
         time.sleep(0.1)
