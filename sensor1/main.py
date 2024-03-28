5d65b98dddbdd8a72a47ee585205bcc19440f791
Florran <florenius05@gmail.com>
2024-03-28 19:43:29 +0100

    Fixed stupid code...

--- sensor1/main.py
+++ sensor1/main.py
@@ -6,6 +6,7 @@ import threading
 pir = MotionSensor(4)
 app = Flask(__name__)
 lock = threading.Lock()
+got_time_remaining = False
 monitoring = False
 motion_detected = False
 reset_requested = False
@@ -14,7 +15,6 @@ reset_requested = False
 def handle_request():
     global monitoring
     global motion_detected
-    global reset_requested
     global got_time_remaining
     request_data = request.args.get('data')
 
@@ -33,15 +33,16 @@ def handle_request():
             return 'Motion detection deactivated!'
         
         elif request_data == "reset":
-            reset_requested = True
+            global reset_requested
             reset()
             return 'Reset requested!'
+
+
         
         return 'Invalid command or already in desired state.'
 
 def monitor_motion():
     global motion_detected
-    global reset_requested
     global monitoring
 
     while monitoring:
@@ -50,7 +51,7 @@ def monitor_motion():
                 print("Motion detected!")
                 motion_detected = True
                 monitoring = False
-        time.sleep(0.01)
+        time.sleep(0.001)
     return
 
 def when_motion_detected():
@@ -60,6 +61,7 @@ def when_motion_detected():
         if motion_detected:
             print("Motion detected!")
             break
+        time.sleep(0.01)
     return
 
 def format_time(milliseconds):
@@ -68,34 +70,23 @@ def format_time(milliseconds):
     milliseconds = milliseconds % 1000
     return f"{minutes:02}:{seconds:02}.{milliseconds:03}"
 
-def timer():
-    start_time = time.time() * 1000
-    while motion_detected:
-        elapsed_time = int((time.time() * 1000) - start_time)
-        print(format_time(elapsed_time), end='\r')
-        time.sleep(0.1)
-        if reset_requested:
-            return
-
 def countdown_timer(seconds):
     global got_time_remaining
-    start_time = time.time()
+    start_time = time.perf_counter()
     end_time = start_time + seconds
     got_time_remaining = True
-    while time.time() < end_time and not reset_requested:
+    while time.perf_counter() < end_time and got_time_remaining:
         remaining_time = end_time - time.time()
         print(format_time(int(remaining_time * 1000)), end='\r')
-        time.sleep(0.001)
+        time.sleep(0.0001)
     got_time_remaining = False
     return
 
 
 def reset():
-    global reset_requested
     global monitoring
     global motion_detected
     global got_time_remaining
-    reset_requested = False
     monitoring = False
     motion_detected = False
     got_time_remaining = False
