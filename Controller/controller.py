a3dbb0c88a14a16288c723cd17c0896812ef65e0
Florran <florenius05@gmail.com>
2024-03-28 23:22:21 +0100

    Reverted prev changes

--- Controller/controller.py
+++ Controller/controller.py
@@ -8,24 +8,25 @@ freeze_time = False
 lock = threading.Lock()
 
 @app.route('/catch_time', methods=['POST'])
-def CatchTime():
-    timeOfMotion = request.json.get('timeOfMotion')
-    if timeOfMotion is not None:
-        timeOfMotion = float(timeOfMotion)
-        threading.Thread(target=Timer, args=(timeOfMotion,)).start()
-        return 'Time received and stopwatch started', 200
-    else:
-        return 'timeOfMotion is of type None', 400
+def catch_time():
+    with lock:
+        time_of_motion = request.json.get('timeOfMotion')
+        time_of_motion = float(time_of_motion)
+        if time_of_motion is not None:
+            threading.Thread(target=timer, args=(time_of_motion,)).start()
+            return 'Time received and stopwatch started', 200
+        else:
+            return 'timeOfMotion is of type None', 400
 
-def Timer(startTime):
-    global freezeTime
-    freezeTime = False
-    while not freezeTime:
-        elapsedTime = time.perf_counter() - startTime
-        print(FormatTime(int(elapsedTime * 1000)), end='\r') # Convert seconds to milliseconds
+def timer(start_time):
+    global freeze_time
+    freeze_time = False
+    while not freeze_time:
+        elapsed_time = time.perf_counter() - start_time
+        print(format_time(int(elapsed_time * 1000)), end='\r') # Convert seconds to milliseconds
         time.sleep(0.0001)
 
-def FormatTime(milliseconds):
+def format_time(milliseconds):
     minutes = (milliseconds % 3600000) // 60000
     seconds = (milliseconds % 60000) // 1000
     milliseconds = milliseconds % 1000
--- Sensor1/sensor1.py
+++ Sensor1/sensor1.py
@@ -4,80 +4,81 @@ import time
 import threading
 import requests
 
-Pir = MotionSensor(4)
-App = Flask(__name__)
-Lock = threading.Lock()
-GotTimeRemaining = False
-Monitoring = False
-MotionDetected = False
-ResetRequested = False
-ControllerEndpoint = "http://192.168.175.96:5000"
+pir = MotionSensor(4)
+app = Flask(__name__)
+lock = threading.Lock()
+got_time_remaining = False
+monitoring = False
+motion_detected = False
+reset_requested = False
+main_endpoint = "http://192.168.175.96/catch_time"
 
-@App.route('/HandleRequest', methods=['GET'])
-def HandleRequest():
-    global Monitoring
-    global MotionDetected
-    global GotTimeRemaining
-    RequestData = request.args.get('data')
+@app.route('/handle_request', methods=['GET'])
+def handle_request():
+    global monitoring
+    global motion_detected
+    global got_time_remaining
+    request_data = request.args.get('data')
 
-    with Lock:
-        if RequestData == "activate" and not Monitoring:
-            Monitoring = True
+    with lock:
+        if request_data == "activate" and not monitoring:
+            monitoring = True
             print("Motion detection activated!")
-            threading.Thread(target=CountdownTimer, args=(60,)).start()
-            threading.Thread(target=MonitorMotion).start()
+            threading.Thread(target=countdown_timer, args=(60,)).start()
+            threading.Thread(target=monitor_motion).start()
             return 'Motion detection activated!'
         
-        elif RequestData == "reset":
-            global ResetRequested
-            Reset()
+        elif request_data == "reset":
+            global reset_requested
+            reset()
             return 'Reset requested!'
         return 'Invalid command or already in desired state.'
 
-def MonitorMotion():
-    global MotionDetected
-    global Monitoring
+def monitor_motion():
+    global motion_detected
+    global monitoring
 
-    while Monitoring and GotTimeRemaining:
-        if Pir.motion_detected:
-            with Lock:
+    while monitoring and got_time_remaining:
+        if pir.motion_detected:
+            with lock:
                 print("Motion detected!")
-                MotionDetected = True
-                Monitoring = False
-                SendTimeOfMotion()
+                motion_detected = True
+                monitoring = False
+                send_time_of_motion()
 
         time.sleep(0.001)
     return
 
-def FormatTime(milliseconds):
-    Minutes = (milliseconds % 3600000) // 60000
-    Seconds = (milliseconds % 60000) // 1000
-    Milliseconds = milliseconds % 1000
-    return f"{Minutes:02}:{Seconds:02}.{Milliseconds:03}"
+def format_time(milliseconds):
+    minutes = (milliseconds % 3600000) // 60000
+    seconds = (milliseconds % 60000) // 1000
+    milliseconds = milliseconds % 1000
+    return f"{minutes:02}:{seconds:02}.{milliseconds:03}"
 
-def CountdownTimer(seconds):
-    global GotTimeRemaining
-    StartTime = time.perf_counter()
-    EndTime = StartTime + seconds
-    GotTimeRemaining = True
-    while time.perf_counter() < EndTime and GotTimeRemaining:
-        RemainingTime = EndTime - time.perf_counter()
-        print(FormatTime(int(RemainingTime * 1000)), end='\r') #Convert seconds to milliseconds
+def countdown_timer(seconds):
+    global got_time_remaining
+    start_time = time.perf_counter()
+    end_time = start_time + seconds
+    got_time_remaining = True
+    while time.perf_counter() < end_time and got_time_remaining:
+        remaining_time = end_time - time.perf_counter()
+        print(format_time(int(remaining_time * 1000)), end='\r') #Convert seconds to milliseconds
         time.sleep(0.0001)
-    GotTimeRemaining = False
+    got_time_remaining = False
     return
 
-def SendTimeOfMotion():
-    TimeOfMotion = time.perf_counter()
-    requests.post(ControllerEndpoint + '/catch_time', json={'timeOfMotion': TimeOfMotion})
+def send_time_of_motion():
+    time_of_motion = time.perf_counter()
+    requests.post('http://192.168.175.96:5000/catch_time', json={'timeOfMotion': time_of_motion})
 
-def Reset():
-    global Monitoring
-    global MotionDetected
-    global GotTimeRemaining
-    Monitoring = False
-    MotionDetected = False
-    GotTimeRemaining = False
+
+def reset():
+    global monitoring
+    global motion_detected
+    global got_time_remaining
+    monitoring = False
+    motion_detected = False
+    got_time_remaining = False
 
 if __name__ == '__main__':
-    App.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
